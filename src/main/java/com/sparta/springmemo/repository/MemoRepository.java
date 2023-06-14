183fdd3eaef4978566bbdf89bd46a2737d91c73a
HyungCheolSim <sim34122@gmail.com>
2023-06-14 17:54:06 +0900

    what is query method?

--- src/main/java/com/sparta/springmemo/repository/MemoRepository.java
+++ src/main/java/com/sparta/springmemo/repository/MemoRepository.java
@@ -3,6 +3,10 @@ package com.sparta.springmemo.repository;
 import com.sparta.springmemo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //jpa repository를 extends 함으로써 @repository 어노테이션이 붙은 효과
 public interface MemoRepository extends JpaRepository<Memo,Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
 }
