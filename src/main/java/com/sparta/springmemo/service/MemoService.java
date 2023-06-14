4ccc876cc02f0aa04482586df3e6a7bc7a151992
HyungCheolSim <sim34122@gmail.com>
2023-06-14 11:34:00 +0900

    fix duplicate code

--- src/main/java/com/sparta/springmemo/service/MemoService.java
+++ src/main/java/com/sparta/springmemo/service/MemoService.java
@@ -9,31 +9,24 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class MemoService {
-
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoRepository memoRepository;
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository=new MemoRepository(jdbcTemplate);
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
         // DB 저장
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
-        memoRepository.save(memo);
-
-
-        // Entity -> ResponseDto
-        return new MemoResponseDto(memo);
+        Memo saveMemo=memoRepository.save(memo);
+        return new MemoResponseDto(saveMemo);
     }
 
     public List<MemoResponseDto> getMemos() {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
         return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
@@ -45,7 +38,6 @@ public class MemoService {
     }
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository=new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
